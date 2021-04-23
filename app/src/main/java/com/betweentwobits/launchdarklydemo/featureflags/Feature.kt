4ef5ba76d097454a13510092f60ef497a5153e7d
Brandon Davis <brandon.davis1@nbcuni.com>
2021-04-23 10:04:16 -0600

    remove explanation

--- app/src/main/java/com/betweentwobits/launchdarklydemo/featureflags/Feature.kt
+++ app/src/main/java/com/betweentwobits/launchdarklydemo/featureflags/Feature.kt
@@ -3,9 +3,8 @@ package com.betweentwobits.launchdarklydemo.featureflags
 sealed class Feature(
     val key: String,
     val title: String,
-    val explanation: String,
     val defaultValue: Boolean
 ) {
-    object NewFeatureOne : Feature("test-flag-1", "Test Feature 1", "Enables test feature 1", false)
-    object NewFeatureTwo : Feature("test-flag-2", "Test Feature 2", "Enables test feature 2", false)
+    object NewFeatureOne : Feature("test-flag-1", "Test Feature 1", false)
+    object NewFeatureTwo : Feature("test-flag-2", "Test Feature 2", false)
 }
